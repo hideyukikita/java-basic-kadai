532d7766e4b68e9d72eea6d1284027ff9418a759
hideyukikita <kitatakikamo@gmail.com>
2024-09-24 23:08:14 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,32 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+		private int gear = 1; //ギア表示
+		private int speed = 10; //スピード表示
+		private int afterGear = 1; //変更後
+		
+		public Car_Chapter15(int gear,int speed) {
+			this.gear = gear;
+			this.speed = speed;
+		}
+		
+		public void gearCharge(int afterGear) {
+			this.afterGear = afterGear;
+			switch(afterGear) {
+			case 1 -> speed = 10;
+			case 2 -> speed = 20;
+			case 3 -> speed = 30;
+			case 4 -> speed = 40;
+			case 5 -> speed = 50;
+			default -> speed = 10;
+			}
+		}
+		
+		public void run() {
+			System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+			System.out.println("速度は" + speed + "です");
+		} 
+	}
+
+
